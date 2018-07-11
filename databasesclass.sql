beb39efa5555aa64fb8f02a56320ca7a204c4405
Jake Sage <jsagehome@gmail.com>
2018-07-11 16:11:48 -0400

    Add files via upload

--- databasesclass.sql
+++ databasesclass.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 08, 2018 at 08:42 PM
+-- Generation Time: Jul 11, 2018 at 08:11 PM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -30,17 +30,16 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `event`;
 CREATE TABLE IF NOT EXISTS `event` (
-  `event_id` int(10) NOT NULL,
+  `event_id` int(10) NOT NULL AUTO_INCREMENT,
   `university_id` int(10) NOT NULL,
   `name` tinytext NOT NULL,
   `location` text NOT NULL COMMENT 'Location Data stored as string for now',
   `description` text NOT NULL,
-  `date` date NOT NULL,
-  `start_time` time DEFAULT NULL,
-  `end_time` time DEFAULT NULL,
+  `date` datetime NOT NULL,
   `privacy` int(1) NOT NULL COMMENT '0 = public, 1 = university only, 2 = RSO only',
   `approved` tinyint(1) NOT NULL,
   PRIMARY KEY (`event_id`),
+  UNIQUE KEY `no_two_same_time_place` (`location`(50),`date`),
   KEY `at_university` (`university_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -55,8 +54,8 @@ CREATE TABLE IF NOT EXISTS `event_attendence` (
   `event_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL,
   UNIQUE KEY `no_duplicate_attendence` (`event_id`,`user_id`),
-  KEY `event_id` (`event_id`),
-  KEY `user_id` (`user_id`)
+  KEY `user_id` (`user_id`),
+  KEY `attends_event` (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -70,8 +69,8 @@ CREATE TABLE IF NOT EXISTS `host_event` (
   `event_id` int(10) NOT NULL,
   `rso_id` int(10) NOT NULL,
   UNIQUE KEY `dont_host_twice` (`event_id`,`rso_id`),
-  KEY `event_id` (`event_id`),
-  KEY `rso_id` (`rso_id`)
+  KEY `rso_id` (`rso_id`),
+  KEY `event_hosted` (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -82,11 +81,12 @@ CREATE TABLE IF NOT EXISTS `host_event` (
 
 DROP TABLE IF EXISTS `rso`;
 CREATE TABLE IF NOT EXISTS `rso` (
-  `rso_id` int(10) NOT NULL,
+  `rso_id` int(10) NOT NULL AUTO_INCREMENT,
   `university_id` int(10) NOT NULL,
   `name` tinytext NOT NULL,
   `description` text NOT NULL,
-  `membership` int(10) DEFAULT NULL,
+  `membership` int(10) NOT NULL,
+  `active` tinyint(1) NOT NULL,
   `website` tinytext,
   PRIMARY KEY (`rso_id`),
   KEY `with_university` (`university_id`)
@@ -104,8 +104,8 @@ CREATE TABLE IF NOT EXISTS `rso_membership` (
   `user_id` int(10) NOT NULL,
   `admin` tinyint(1) NOT NULL,
   UNIQUE KEY `no_duplicates` (`rso_id`,`user_id`),
-  KEY `rso_id` (`rso_id`),
-  KEY `user_id` (`user_id`)
+  KEY `user_id` (`user_id`),
+  KEY `member_of_rso` (`rso_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Pairs users to the RSOs they are members of and Admin status';
 
 -- --------------------------------------------------------
@@ -116,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `rso_membership` (
 
 DROP TABLE IF EXISTS `university`;
 CREATE TABLE IF NOT EXISTS `university` (
-  `university_id` int(10) NOT NULL,
+  `university_id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `description` text NOT NULL,
   `location` text COMMENT 'We''ll store the location data as a string for now',
@@ -124,7 +124,15 @@ CREATE TABLE IF NOT EXISTS `university` (
   `website` tinytext,
   PRIMARY KEY (`university_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `university`
+--
+
+INSERT INTO `university` (`university_id`, `name`, `description`, `location`, `population`, `website`) VALUES
+(1, 'University of Central Florida', 'Best College', 'Here', 5, NULL),
+(2, 'USFf', 'We\'re not as cool as UCF.', 'yeeeee', 1, 'e');
 
 -- --------------------------------------------------------
 
@@ -134,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `university` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
   `university_id` int(10) NOT NULL,
   `email` tinytext NOT NULL,
   `password` tinytext NOT NULL,
@@ -143,7 +151,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`(50)),
   KEY `attends_university` (`university_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='table for users with accounts';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='table for users with accounts';
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `university_id`, `email`, `password`, `name`, `permission_level`) VALUES
+(1, 1, 'e.mail@email.mail', '12345', 'Joseph Test', 0),
+(2, 1, 'email@e.mail', '23456', 'Jonny Test', 0);
 
 -- --------------------------------------------------------
 
@@ -153,7 +169,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 DROP TABLE IF EXISTS `writes_comment_on`;
 CREATE TABLE IF NOT EXISTS `writes_comment_on` (
-  `comment_id` int(10) NOT NULL,
+  `comment_id` int(10) NOT NULL AUTO_INCREMENT,
   `event_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL,
   `title` tinytext NOT NULL,
